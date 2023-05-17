dc1108376e5d83767060c9fa5e002a985a18f17b
Luigi213 <reyesgianluigi88@gmail.com>
2023-05-17 10:26:03 +0200

    milestone 1 done

---
+++ src/org/java/Evento.java
@@ -0,0 +1,67 @@
+package org.java;
+
+import java.time.LocalDate;
+
+public class Evento {
+	private String titolo;
+	private LocalDate data;
+	private int numPosTot;
+	private int numPosPre;
+	
+	public Evento (String titolo, LocalDate data) throws Exception {
+		setTitolo(titolo);
+		setData(data);
+		numPosTot = 100;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) throws Exception {
+		if (LocalDate.of(2022, 1, 10).isAfter(data)) {			
+			throw new Exception("Data passata");
+		}
+		this.data = data;
+	}
+	public int getNumPosTot() {
+		return numPosTot;
+	}
+	public int getNumPosPre() {
+		return numPosPre;
+	}
+	
+	public void prenota() throws Exception {
+		if (LocalDate.now().isAfter(getData())) {			
+			throw new Exception("Data passata non puoi prenotare");
+		}
+		if(getNumPosPre() > getNumPosTot()) {
+			throw new Exception("Posti esauriti");
+		}
+		numPosPre = getNumPosPre() + 1; 
+		System.out.println("Hai prenotato");
+	}
+	public void disdici() throws Exception {
+		if (LocalDate.now().isAfter(getData())) {			
+			throw new Exception("Data passata non puoi prenotare");
+		}
+		if(getNumPosPre() == 0) {
+			throw new Exception("Non hai prenotato niente");
+		}
+		numPosPre = getNumPosPre() - 1; 
+		System.out.println("Hai disdetto");
+	}
+	
+	@Override
+	public String toString() {
+		return "titolo dell'evento: " + getTitolo()
+				+ "\ndata dell'evento: " + getData()
+				+ "\nnumero di posti totali: " + getNumPosTot()
+				+ "\nnumero di posti prenotati: " + getNumPosPre();
+	}
+}
--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -1,7 +1,19 @@
 package org.java;
 
+import java.time.LocalDate;
+
 public class Main {
 	public static void main(String[] args) {
+		
+		try {			
+			Evento e1 = new Evento("ciao", LocalDate.of(2025, 05, 12));
+			e1.prenota();
+			e1.disdici();
+			
+			System.out.println(e1);
+		} catch (Exception e) {
+			System.err.println("Errore nella lettura della data, " + e.getMessage());
+		}
 		
 	}
 }
